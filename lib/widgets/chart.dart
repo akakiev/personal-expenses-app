c54bd61ef4a28e6b0c1f64509561154a22fad700
Serhii Baraban <akakieva@gmail.com>
2023-02-06 15:06:08 +0100

    created Bars for the Chart

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -34,7 +34,9 @@ class Chart extends StatelessWidget {
       elevation: 6,
       margin: EdgeInsets.all(20),
       child: Row(
-        children: <Widget>[],
+        children: groupedTransactionValues.map((data) {
+          return Text('${data['day']}: ${data['amount']}');
+        }).toList(),
       ),
     );
   }
