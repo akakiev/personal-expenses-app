941e690d40d7cf2cbebcad962d8c9a8a24b23087
Serhii Baraban <akakieva@gmail.com>
2023-04-03 12:48:35 +0200

    calculated the sizes automatically, used the layout builder widget

--- lib/main.dart
+++ lib/main.dart
@@ -91,25 +91,36 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'Personal Expensive App',
-          //style: TextStyle(fontFamily: 'QuickSand')
-        ),
-        actions: <Widget>[
-          IconButton(
-              onPressed: () => _startAddNewTransaction(context),
-              icon: Icon(Icons.add))
-        ],
+    final appBar = AppBar(
+      title: Text(
+        'Personal Expensive App',
+        //style: TextStyle(fontFamily: 'QuickSand')
       ),
+      actions: <Widget>[
+        IconButton(
+            onPressed: () => _startAddNewTransaction(context),
+            icon: Icon(Icons.add))
+      ],
+    );
+    return Scaffold(
+      appBar: appBar,
       body: SingleChildScrollView(
         child: Column(
             //mainAxisAlignment: MainAxisAlignment.start,
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
-              Chart(_recentTransactions),
-              TransactionList(_userTransactions, _deleteTransaction)
+              Container(
+                  height: (MediaQuery.of(context).size.height -
+                          appBar.preferredSize.height -
+                          MediaQuery.of(context).padding.top) *
+                      0.3,
+                  child: Chart(_recentTransactions)),
+              Container(
+                  height: (MediaQuery.of(context).size.height -
+                          appBar.preferredSize.height -
+                          MediaQuery.of(context).padding.top) *
+                      0.7,
+                  child: TransactionList(_userTransactions, _deleteTransaction))
             ]),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -9,50 +9,47 @@ class TransactionList extends StatelessWidget {
   TransactionList(this.transactions, this.deleteTx);
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 400,
-      child: transactions.isEmpty
-          ? Column(children: <Widget>[
-              Text(
-                'No Transactions added yet!',
-                style: Theme.of(context).textTheme.titleMedium,
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Container(
-                  height: 200,
-                  child: Image.asset('assets/images/waiting.png',
-                      fit: BoxFit.cover)),
-            ])
-          : ListView.builder(
-              itemBuilder: (ctx, index) {
-                return Card(
-                  elevation: 5,
-                  margin: EdgeInsets.symmetric(vertical: 8, horizontal: 5),
-                  child: ListTile(
-                    leading: CircleAvatar(
-                        radius: 30,
-                        child: Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: FittedBox(
-                              child: Text('\$${transactions[index].amount}')),
-                        )),
-                    title: Text(transactions[index].title,
-                        style: Theme.of(context).textTheme.titleMedium),
-                    subtitle: Text(
-                        DateFormat.yMMMd().format(transactions[index].date)),
-                    trailing: IconButton(
-                      icon: Icon(Icons.delete),
-                      color: Colors.red,
-                      onPressed: () => deleteTx(transactions[index].id),
-                    ),
-                  ),
-                );
-              },
-              itemCount: transactions.length,
-              //children: transactions.map((tx) {  }).toList()),
+    return transactions.isEmpty
+        ? Column(children: <Widget>[
+            Text(
+              'No Transactions added yet!',
+              style: Theme.of(context).textTheme.titleMedium,
             ),
-    );
+            SizedBox(
+              height: 10,
+            ),
+            Container(
+                height: 200,
+                child: Image.asset('assets/images/waiting.png',
+                    fit: BoxFit.cover)),
+          ])
+        : ListView.builder(
+            itemBuilder: (ctx, index) {
+              return Card(
+                elevation: 5,
+                margin: EdgeInsets.symmetric(vertical: 8, horizontal: 5),
+                child: ListTile(
+                  leading: CircleAvatar(
+                      radius: 30,
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: FittedBox(
+                            child: Text('\$${transactions[index].amount}')),
+                      )),
+                  title: Text(transactions[index].title,
+                      style: Theme.of(context).textTheme.titleMedium),
+                  subtitle:
+                      Text(DateFormat.yMMMd().format(transactions[index].date)),
+                  trailing: IconButton(
+                    icon: Icon(Icons.delete),
+                    color: Colors.red,
+                    onPressed: () => deleteTx(transactions[index].id),
+                  ),
+                ),
+              );
+            },
+            itemCount: transactions.length,
+            //children: transactions.map((tx) {  }).toList()),
+          );
   }
 }
