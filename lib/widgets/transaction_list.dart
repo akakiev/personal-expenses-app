a7dd92caa445df32c8091e23c80a4b2336bd497d
Serhii Baraban <akakieva@gmail.com>
2023-04-04 16:05:33 +0200

    added the landscape mode

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -10,19 +10,21 @@ class TransactionList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return transactions.isEmpty
-        ? Column(children: <Widget>[
-            Text(
-              'No Transactions added yet!',
-              style: Theme.of(context).textTheme.titleMedium,
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Container(
-                height: 200,
-                child: Image.asset('assets/images/waiting.png',
-                    fit: BoxFit.cover)),
-          ])
+        ? LayoutBuilder(builder: (ctx, constraints) {
+            return Column(children: <Widget>[
+              Text(
+                'No Transactions added yet!',
+                style: Theme.of(context).textTheme.titleMedium,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                  height: constraints.maxHeight * 0.6,
+                  child: Image.asset('assets/images/waiting.png',
+                      fit: BoxFit.cover)),
+            ]);
+          })
         : ListView.builder(
             itemBuilder: (ctx, index) {
               return Card(
